41a5bd2d8a08a36ba5c3f3b6c392240487d4e17a
Avi <avinash03082003@gmail.com>
2023-07-14 02:41:24 +0530

    Added Tree Traversal Solutions

---
+++ 112. PreOrder Traversal Solution.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h> 
+using namespace std;
+class TreeNode{
+    public:
+    int data;
+    TreeNode *left, *right;
+};
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+void traverse(vector<int> &v , TreeNode* root){
+    if(root){
+        v.push_back(root->data);
+        traverse(v,root->left);
+        traverse(v,root->right);
+    }
+}
+vector<int> getPreOrderTraversal(TreeNode *root)
+{
+    // Write your code here.
+    vector<int> v;
+    traverse(v,root);
+    return v;
+}
